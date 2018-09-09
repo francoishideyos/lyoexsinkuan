b0cf02bc34d25bbdfe07e81f290f6d205abe2b79
Lyoe <lyoeleex@gmail.com>
2018-09-09 20:41:09 +0800

    Change

--- lyoexsinkuan/settings.py
+++ lyoexsinkuan/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/dev/ref/settings/
 """
 
 import os
+from .email_info import *
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -31,7 +32,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'lyoexsinkuan@gmail.com'
-EMAIL_HOST_PASSWORD = 'Lee-tleindi@'
+EMAIL_HOST_PASSWORD = EMAIL_HOST_PASSWORD
 EMAIL_PORT = 587
 
 
