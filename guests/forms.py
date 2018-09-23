265687a4d0f0da75d997902510e2c0a650ecf0c6
Lyoe <lyoeleex@gmail.com>
2018-09-24 00:08:27 +0800

    added fields n rsvp

--- guests/forms.py
+++ guests/forms.py
@@ -4,15 +4,28 @@ from django.contrib.auth.models import User
 from django.core.validators import RegexValidator
 from .models import Guest
 
+#https://stackoverflow.com/questions/44187640/django-1-11-horizontal-choice-field
+class HorizontalRadioRenderer(forms.RadioSelect):
+    template_name = 'horizontal_select.html'
+    
+    # def __init__(self, *args, **kwargs):
+    #     super().__init__(*args, **kwargs)
+    #     css_style = 'style="display: inline-block; margin-right: 10px;"'
+    #     self.renderer.inner_html = '<li ' + css_style + '>{choice_value}{sub_widgets}</li>'
+
+    # def render(self):
+    #  return mark_safe(u'\n'.join([u'%s\n' % w for w in self]))
+
 class SignUpForm(forms.ModelForm):
-    # first_name = forms.CharField(max_length=30, required=False )
+    # name = forms.CharField(required=True)
     # last_name = forms.CharField(max_length=30, required=False )
     # email = forms.EmailField(max_length=254, required=True,help_text="*")
     # phone_regex = RegexValidator(regex=r'^\d{9,15}$', message="Format: AreaCode+Number - 6592962824.")
     # whatsapp_phone_number = forms.CharField(help_text='Format: 85291790376',max_length=30, validators=[phone_regex]) # validators should be a list
     
     # def __init__(self, *args, **kwargs):
-        # super(SignUpForm, self).__init__(*args, **kwargs)
+    #     super(SignUpForm, self).__init__(*args, **kwargs)
+    #     self.fields['comments'].widget = forms.TextInput(attrs={'placeholder': 'Allergies/Children/Others'})
         
         # https://stackoverflow.com/questions/14299039/django-how-to-remove-the-password-field-from-the-usercreationform/14301395
         # del self.fields['password1']
@@ -26,4 +39,14 @@ class SignUpForm(forms.ModelForm):
     class Meta:
     	# https://developer.mozilla.org/en-US/docs/Learn/Server-side/Django/Forms
         model = Guest
-        fields = ('email','first_name', 'last_name','whatsapp_phone_number')
+        fields = ('email','name','whatsapp_number', 'is_attending', 'comments')
+        
+        # for radio buttons
+        # https://stackoverflow.com/questions/30199471/how-to-specify-select-and-radioselect-in-a-modelform
+        widgets = {
+            'email': forms.TextInput(attrs={'size': 30}),
+            'name': forms.TextInput(attrs={'placeholder':'First Name, Last Name','size': 30}),
+            'is_attending': HorizontalRadioRenderer,
+            'whatsapp_number': forms.TextInput(attrs={'placeholder': 'e.g. 85291790376 (Areacode + number, NO "-"/spaces)', 'size': 50}),
+            'comments': forms.TextInput(attrs={'placeholder': 'Allergies/Food Preferences/Children/Others', 'size': 60})
+        }
--- guests/models.py
+++ guests/models.py
@@ -15,18 +15,24 @@ class Guest(models.Model):
     """
     A single guest
     """
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
+    STATE_CHOICES = (
+        (True, u'Will Attend'),
+        (False, u'Will Not Attend'),
+    )
+
+    name = models.CharField(max_length=50)
+    # last_name = models.CharField(max_length=30)
     email = models.EmailField(max_length=80)
     phone_regex = RegexValidator(regex=r'^\d{9,15}$', message="Format: AreaCode+Number - 6592962824.")
-    whatsapp_phone_number = models.CharField(default='0',help_text='Format: 85291790376',max_length=30, validators=[phone_regex]) # validators should be a list
-    is_attending = models.NullBooleanField(default=None)
-    is_child = models.BooleanField(default=False)
+    whatsapp_number = models.CharField(max_length=13) # validators should be a list
+    is_attending = models.BooleanField(default=None,choices=STATE_CHOICES)
+    # is_child = models.BooleanField(default=False)
+    comments = models.CharField(max_length=1000)
 
-    @property
-    def name(self):
-        return u'{} {}'.format(self.first_name, self.last_name)
+    # @property
+    # def name(self):
+    #     return u'{} {}'.format(self.first_name, self.last_name)
 
-    def __unicode__(self):
-        return 'Guest: {} {}'.format(self.first_name, self.last_name)
+    # def __unicode__(self):
+    #     return 'Guest: {} {}'.format(self.first_name, self.last_name)
 
