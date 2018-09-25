ac56fa7a7313b0570fa600ebf7150e019110c530
Lyoe <lyoeleex@gmail.com>
2018-09-25 22:13:43 +0800

    push

--- lyoexsinkuan/email_info.py
+++ lyoexsinkuan/email_info.py
@@ -1,5 +1,7 @@
+import os
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'lyoexsinkuan@gmail.com' 
+EMAIL_HOST_PASSWORD = os.environ.get(EMAIL_HOST_PASSWORD)
 EMAIL_PORT = 587
