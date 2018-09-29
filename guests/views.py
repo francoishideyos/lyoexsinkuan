5490d8341bf1db3d10cef9b85db0aaec1f7086b8
Lyoe <lyoeleex@gmail.com>
2018-09-29 23:45:54 +0800

    trying advanced link

--- guests/views.py
+++ guests/views.py
@@ -6,6 +6,7 @@ from django.core.mail import send_mail
 from django.template.loader import get_template
 from .forms import SignUpForm
 from django.contrib.auth import login, authenticate
+from email.mime.text import MIMEText
 
 # from .models import Party
 
@@ -24,9 +25,11 @@ def rsvp(request):
             post.user = request.user
             post.save()
             # https://www.youtube.com/watch?v=51mmqf5a0Ss
+            # if anything wrong with email reply (SMTP error)
+            # https://accounts.google.com/DisplayUnlockCaptcha
             # send_mail(subject, message, from_email, to_list, fail_silently=True)
             subject = "Thank you!"
-            message_text = "Dear " + str(post.name) + ", \nThank you for RSVP-ing, hope to see you on May 18th, 2019 at <a href='https://goo.gl/maps/QrJoixZJDNu' target='_blank'>Map</a>"
+            message_text = "Dear " + str(post.name) + ", \nThank you for RSVP-ing, hope to see you on May 18th, 2019 at " + MIMEText(u"<a href='https://goo.gl/maps/QrJoixZJDNu' target='_blank'>Map</a>",'html')
             
             from_email = settings.EMAIL_HOST_USER
             to_list = [post.email, settings.EMAIL_HOST_USER]
