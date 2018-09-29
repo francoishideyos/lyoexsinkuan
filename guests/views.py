814b822f7a0389c17b4eb072ba6054e461b24253
Lyoe <lyoeleex@gmail.com>
2018-09-29 23:58:07 +0800

    email advanced

--- guests/views.py
+++ guests/views.py
@@ -2,11 +2,11 @@ from django.conf import settings
 from django.contrib import messages
 from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
-from django.core.mail import send_mail
+from django.core.mail import send_mail, EmailMultiAlternatives
 from django.template.loader import get_template
 from .forms import SignUpForm
 from django.contrib.auth import login, authenticate
-from email.mime.text import MIMEText
+
 
 # from .models import Party
 
@@ -29,12 +29,17 @@ def rsvp(request):
             # https://accounts.google.com/DisplayUnlockCaptcha
             # send_mail(subject, message, from_email, to_list, fail_silently=True)
             subject = "Thank you!"
-            message_text = "Dear " + str(post.name) + ", \nThank you for RSVP-ing, hope to see you on May 18th, 2019 at " + MIMEText(u"<a href='https://goo.gl/maps/QrJoixZJDNu' target='_blank'>Map</a>",'html')
-            
+            message_text = "Dear " + str(post.name) + ", \nThank you for RSVP-ing, hope to see you on May 18th, 2019 at Angel's Share Cafe."
+            message_html = "<p>Dear " + str(post.name) + ", \nThank you for RSVP-ing, hope to see you on May 18th, 2019 at <a href='https://goo.gl/maps/QrJoixZJDNu' target='_blank'>Angel's Share Cafe</a> <p>"
             from_email = settings.EMAIL_HOST_USER
             to_list = [post.email, settings.EMAIL_HOST_USER]
             
-            send_mail(subject, message_text, from_email, to_list,fail_silently=False)
+            msg = EmailMultiAlternatives(subject, message_text, from_email, to_list)
+            msg.attach_alternative(message_html, "text/html")
+            msg.send(fail_silently=False)
+
+            # send_mail(subject, message_text, from_email, to_list,fail_silently=False)
+
             messages.success(request, 'Thank you for RSVP-ing, looking forward to see you!')
             
             return redirect('../')
